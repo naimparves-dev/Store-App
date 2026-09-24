15a7470c4b6de0735496e6ae4562a464857c6f33
naimparves-dev <mdnaimparves3@gmail.com>
2026-09-25 03:04:36 +0600

    done

--- src/lib/Apps.ts
+++ src/lib/Apps.ts
@@ -1,5 +1,8 @@
-export const getApps=async()=>{
-    const res=await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data.json`)
-    const data=await res.json();
-    return data
-}
+export const getApps = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/data.json`
+  );
+
+  const data = await res.json();
+  return data;
+};
