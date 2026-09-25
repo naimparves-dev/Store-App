bf746fc504559caea5a38f700e6a410e70554976
naimparves-dev <mdnaimparves3@gmail.com>
2026-09-25 18:24:04 +0600

    fix

--- src/lib/Apps.ts
+++ src/lib/Apps.ts
@@ -1,7 +1,7 @@
 export const getApps = async () => {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/data.json`
+      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/data.json`
     );
 
     if (!res.ok) {
